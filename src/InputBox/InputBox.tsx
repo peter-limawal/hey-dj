35d88d70c5a7e39b5224aa021a56607596a69ef4
Peter Limawal <peterlimawal007@gmail.com>
2023-03-18 22:44:52 -0400

    frontend basic design commit

---
+++ src/InputBox/InputBox.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import './InputBox.css'
+
+interface InputBoxProps {
+  onInputSubmit: (input: string) => void
+}
+
+export const InputBox: React.FC<InputBoxProps> = ({ onInputSubmit }) => {
+  const [input, setInput] = useState('')
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+    onInputSubmit(input)
+    setInput('')
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="input-form">
+      <label htmlFor="input-box" className="input-label">
+        Tell me what you want to listen to:
+      </label>
+      <input
+        id="input-box"
+        type="text"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+        className="input-field"
+      />
+      <button type="submit" className="submit-button">
+        Submit
+      </button>
+    </form>
+  )
+}
+
+export default InputBox
