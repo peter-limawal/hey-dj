9e41ede4e89dc3ecfda171c31181df47a4e4dae9
Peter Limawal <peterlimawal007@gmail.com>
2023-03-19 02:14:59 -0400

    integrate gpt-3.5-turbo

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express'
 import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3001
 
-// Add these two lines to enable CORS and parse JSON request bodies
+// OpenAI API key
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
+
 app.use(cors())
 app.use(express.json())
 
@@ -12,10 +21,47 @@ app.get('/', (req, res) => {
   res.send('Hello from the backend server!')
 })
 
-// Add this new route to handle the input
-app.post('/input', (req, res) => {
-  console.log('Received input:', req.body.input)
-  res.send({ message: 'Input received' })
+async function getSongListFromGPT(input: string): Promise<string[]> {
+  console.log('getSongListFromGPT called with input:', input)
+
+  try {
+    const gptResponse = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content:
+            'You are an AI that can suggest a list of songs based on user input. Generate a list of 20 songs',
+        },
+        {
+          role: 'user',
+          content: input,
+        },
+      ],
+      max_tokens: 300,
+      n: 1,
+      temperature: 0.8,
+    })
+
+    // Log the raw response from the GPT API
+    console.log('GPT API response:', gptResponse.data)
+
+    const songsText = gptResponse.data.choices[0].message?.content || ''
+    const songs = songsText
+      .split('\n')
+      .filter((song: string) => song.trim() !== '')
+
+    return songs
+  } catch (error) {
+    console.error('Error calling the GPT API:', error)
+    return []
+  }
+}
+
+app.post('/input', async (req, res) => {
+  const input = req.body.input
+  const songs = await getSongListFromGPT(input)
+  res.send({ songs })
 })
 
 app.listen(port, () => {
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [input, setInput] = useState('')
+  const [songs, setSongs] = useState<string[]>([])
+
+  const handleSubmit = async () => {
+    const response = await fetch('http://localhost:3001/input', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ input }),
+    })
+
+    const data = await response.json()
+    console.log(data)
+    setSongs(data.songs)
+  }
+
   return (
     <div className="container">
       <h1>AI DJ</h1>
-      <input type="text" placeholder="Type your mood or song request here" />
-      <button>Submit</button>
+      <input
+        type="text"
+        placeholder="Type your song request here"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+      {songs.length > 0 && (
+        <div>
+          <h2>Suggested Songs:</h2>
+          <ul>
+            {songs.map((song: string, index: number) => (
+              <li key={index}>{song}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
