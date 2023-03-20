9040ba300fcd704686924c527f446e5b71ebb453
Peter Limawal <peterlimawal007@gmail.com>
2023-03-20 02:04:54 -0400

    implement user authorization for spotify

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai'
 import dotenv from 'dotenv'
+import querystring from 'querystring'
+import fetch from 'node-fetch'
 
 dotenv.config()
 
@@ -13,6 +15,10 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 })
 const openai = new OpenAIApi(configuration)
+
+const spotifyClientId = process.env.SPOTIFY_CLIENT_ID
+const spotifyClientSecret = process.env.SPOTIFY_CLIENT_SECRET
+const redirectUri = 'http://localhost:5173/auth/callback'
 
 app.use(cors())
 app.use(express.json())
@@ -30,9 +36,9 @@ async function getSongListFromGPT(input: string): Promise<string[]> {
       messages: [
         {
           role: 'system',
-          content: `You are an AI that can suggest a list of songs based on user input. Consistently generate a list of 20 songs following the "Song Name - Artist" format. Do not include any additional text, numbering, or variations in the output. Provide the list of songs in the exact format requested:
+          content: `You are an AI that can suggest a list of songs based on user input. Consistently generate a list of 15-20 songs following the "Song Name - Artist" format. Do not include any additional text, numbering, or variations in the output. Provide the list of songs in the exact format requested:
 
-          List 20 songs below:
+          List songs below:
           
           Song Name - Artist
           Song Name - Artist
@@ -70,6 +76,113 @@ app.post('/input', async (req, res) => {
   const input = req.body.input
   const songs = await getSongListFromGPT(input)
   res.send({ songs })
+})
+
+function generateRandomString(length: number): string {
+  let text = ''
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return text
+}
+
+app.get('/auth/login', (req, res) => {
+  const scope = 'streaming user-read-email user-read-private'
+  const state = generateRandomString(16)
+  const redirect_uri = 'http://localhost:3000/auth/callback'
+
+  const auth_query_parameters = new URLSearchParams({
+    response_type: 'code',
+    client_id: process.env.SPOTIFY_CLIENT_ID as string,
+    scope: scope,
+    redirect_uri: redirect_uri,
+    state: state,
+  })
+
+  res.redirect(
+    'https://accounts.spotify.com/authorize/?' +
+      auth_query_parameters.toString()
+  )
+})
+
+// In-memory storage for tokens
+const userTokens: Record<
+  string,
+  { access_token: string; refresh_token: string }
+> = {}
+
+app.get('/auth/callback', async (req, res) => {
+  const code = req.query.code as string
+
+  if (!code) {
+    res.status(400).send('Authorization code is missing.')
+    return
+  }
+
+  try {
+    const tokenResponse = await fetch(
+      'https://accounts.spotify.com/api/token',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: `Basic ${Buffer.from(
+            `${spotifyClientId}:${spotifyClientSecret}`
+          ).toString('base64')}`,
+        },
+        body: querystring.stringify({
+          grant_type: 'authorization_code',
+          code,
+          redirect_uri: redirectUri,
+        }),
+      }
+    )
+
+    const tokenData: {
+      access_token?: string
+      refresh_token?: string
+    } = (await tokenResponse.json()) as {
+      access_token?: string
+      refresh_token?: string
+    }
+
+    const { access_token: accessToken, refresh_token: refreshToken } = tokenData
+
+    if (!accessToken || !refreshToken) {
+      res.status(400).send('Failed to obtain access token and refresh token.')
+      return
+    }
+
+    // Store the tokens
+    userTokens['currentUser'] = {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+    }
+
+    res.redirect(
+      `http://localhost:3000?access_token=${accessToken}&refresh_token=${refreshToken}`
+    )
+  } catch (error) {
+    console.error('Error during token exchange:', error)
+    res
+      .status(500)
+      .send('Failed to exchange authorization code for access token.')
+  }
+})
+
+app.get('/auth/token', (req, res) => {
+  const currentUserTokens = userTokens['currentUser']
+
+  if (currentUserTokens) {
+    res.json({
+      access_token: currentUserTokens.access_token,
+    })
+  } else {
+    res.status(404).send('No access token found for the current user.')
+  }
 })
 
 app.listen(port, () => {
